4a7d6cc4b5110133a9798863c23810e3daa58811
Maxim Lebedev <git@toby3d.me>
2021-12-30 02:08:09 +0500

    :sparkles: Added HTTP delivery for client package

---
+++ internal/client/delivery/http/home_http_test.go
@@ -0,0 +1,37 @@
+package http_test
+
+import (
+	"testing"
+
+	"github.com/fasthttp/router"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	http "github.com/valyala/fasthttp"
+	"golang.org/x/text/language"
+	"golang.org/x/text/message"
+
+	delivery "source.toby3d.me/website/oauth/internal/client/delivery/http"
+	"source.toby3d.me/website/oauth/internal/domain"
+	"source.toby3d.me/website/oauth/internal/testing/httptest"
+)
+
+func TestRead(t *testing.T) {
+	t.Parallel()
+
+	r := router.New()
+	delivery.NewRequestHandler(
+		domain.TestConfig(t), domain.TestClient(t), language.NewMatcher(message.DefaultCatalog.Languages()),
+	).Register(r)
+
+	client, _, cleanup := httptest.New(t, r.Handler)
+	t.Cleanup(cleanup)
+
+	req := httptest.NewRequest(http.MethodGet, "https://app.example.com/", nil)
+	defer http.ReleaseRequest(req)
+
+	resp := http.AcquireResponse()
+	defer http.ReleaseResponse(resp)
+
+	require.NoError(t, client.Do(req, resp))
+	assert.Equal(t, http.StatusOK, resp.StatusCode())
+}
