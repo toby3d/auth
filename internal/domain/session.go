1d14c7a3472ecc2ba75e525c567b9abda0cf2886
Maxim Lebedev <git@toby3d.me>
2022-01-08 15:52:55 +0500

    :label: Added Session domain

---
+++ internal/domain/session.go
@@ -0,0 +1,27 @@
+package domain
+
+import "testing"
+
+type Session struct {
+	ClientID            *ClientID
+	Me                  *Me
+	RedirectURI         *URL
+	CodeChallengeMethod CodeChallengeMethod
+	Scope               Scopes
+	Code                string
+	CodeChallenge       string
+}
+
+func TestSession(tb testing.TB) *Session {
+	tb.Helper()
+
+	return &Session{
+		ClientID:            TestClientID(tb),
+		Me:                  TestMe(tb),
+		RedirectURI:         TestURL(tb, "https://example.com/callback"),
+		CodeChallengeMethod: CodeChallengeMethodPLAIN,
+		Scope:               Scopes{ScopeProfile, ScopeEmail},
+		Code:                "abcdefg",
+		CodeChallenge:       "hackme",
+	}
+}
